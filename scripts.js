586f44d8a71e2f476df9d4a0100fee23b5585c61
vivian <vivian.v18723@gmail.com>
2022-04-09 14:18:11 +0100

    add ability to move indicator to clicked icon

--- scripts.js
+++ scripts.js
@@ -6,6 +6,56 @@ $("document").ready(function () {
   navLinks.click(function () {
     navLinks.removeClass("active");
     $(this).addClass("active");
+
+    // moves indicator based on presence of respective class
+    if ($(this)[0] === navLinks[0]) {
+      $("li:eq(0)")
+        .parent()
+        .prev()
+        .addClass("move-indicator-1")
+        .removeClass("move-indicator-2")
+        .removeClass("move-indicator-3")
+        .removeClass("move-indicator-4")
+        .removeClass("move-indicator-5");
+    } else if ($(this)[0] === navLinks[1]) {
+      $("li:eq(1)")
+        .parent()
+        .prev()
+        .addClass("move-indicator-2")
+        .removeClass("move-indicator-1")
+        .removeClass("move-indicator-3")
+        .removeClass("move-indicator-4")
+        .removeClass("move-indicator-5");
+    } else if ($(this)[0] === navLinks[2]) {
+      $("li:eq(2)")
+        .parent()
+        .prev()
+        .addClass("move-indicator-3")
+        .removeClass("move-indicator-1")
+        .removeClass("move-indicator-2")
+        .removeClass("move-indicator-4")
+        .removeClass("move-indicator-5");
+    } else if ($(this)[0] === navLinks[3]) {
+      $("li:eq(3)")
+        .parent()
+        .prev()
+        .addClass("move-indicator-4")
+        .removeClass("move-indicator-1")
+        .removeClass("move-indicator-2")
+        .removeClass("move-indicator-3")
+        .removeClass("move-indicator-5");
+    } else if ($(this)[0] === navLinks[4]) {
+      $("li:eq(4)")
+        .parent()
+        .prev()
+        .addClass("move-indicator-5")
+        .removeClass("move-indicator-1")
+        .removeClass("move-indicator-2")
+        .removeClass("move-indicator-3")
+        .removeClass("move-indicator-4");
+    } else {
+      console.log("ERROR!!!!");
+    }
   });
 });
 
