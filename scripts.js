b5f3d2a32fa81ef6f8cae2e9278b756a4db777db
vivian <vivian.v18723@gmail.com>
2022-04-09 03:50:02 +0100

    and js motion to links & html comments

--- scripts.js
+++ scripts.js
@@ -1,3 +1,31 @@
-document.addEventListener("DOMContentLoaded", function () {
-  console.log("document is ready. I can sleep now");
+$("document").ready(function () {
+  // gets link containers
+  const navLinks = $(".link-container");
+
+  // toggles active class presence on anchor links
+  navLinks.click(function () {
+    navLinks.removeClass("active");
+    $(this).addClass("active");
+  });
 });
+
+/* JS only (longer) :
+    document.addEventListener("DOMContentLoaded", function () {
+    // gets link containers
+    const link = document.querySelectorAll(".link-container");
+
+    // toggles w/ active class selector
+    function activatedLink() {
+        link.forEach((singleItem) => {
+        singleItem.classList.remove("active");
+        this.classList.add("active");
+        console.log(link);
+        });
+    }
+
+    // applies func to each link when clicked
+    link.forEach((singleItem) => {
+        singleItem.addEventListener("click", activatedLink);
+    });
+});
+*/
